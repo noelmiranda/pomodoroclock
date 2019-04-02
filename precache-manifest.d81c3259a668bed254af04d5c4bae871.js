b96649b164c8eb042bd3bf34441e871ff656fc9c
nm <noelmiranda19@gmail.com>
2019-04-02 09:22:52 -1100

    Updates

---
+++ precache-manifest.d81c3259a668bed254af04d5c4bae871.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46456d5993a31ea09bf5",
+    "url": "/pomodoroclock/static/js/runtime~main.8b413fb3.js"
+  },
+  {
+    "revision": "5da0e33b233d62d1b42e",
+    "url": "/pomodoroclock/static/js/main.b2e34cc7.chunk.js"
+  },
+  {
+    "revision": "537ceb56e92bc169558d",
+    "url": "/pomodoroclock/static/js/2.80784bb1.chunk.js"
+  },
+  {
+    "revision": "5da0e33b233d62d1b42e",
+    "url": "/pomodoroclock/static/css/main.ef1ac99c.chunk.css"
+  },
+  {
+    "revision": "8ba6206b6713f55b5143fa47dd55fcf3",
+    "url": "/pomodoroclock/index.html"
+  }
+];
